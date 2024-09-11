ae047535db51ddb410db425f4f149dc8593023ce
RominaGiaccio <giacciopromina@gmail.com>
2024-09-11 17:38:03 -0300

    adding collisions and script

--- LoboEstas/Assets/Scripts/PlayerController.cs
+++ LoboEstas/Assets/Scripts/PlayerController.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UIElements;
+using UnityEngine.UIElements;*/
 /*public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 1.0f;
@@ -98,7 +98,7 @@ using UnityEngine.UIElements;
     }
 }*/
 
-
+/*
 public class PlayerController : MonoBehaviour
 {
     public float speed;
@@ -249,4 +249,169 @@ public class PlayerController : MonoBehaviour
     {
         Move_();
     }
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+    public SwordAttack swordAttack;
+    private GameObject interactableCrop;
+    Vector2 movementInput; // 2 valores, X,Y (izq der, arriba abajo)
+    Rigidbody2D rb;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    bool canMove = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        //Movimiento
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+        if (canMove)
+        {
+            if (movementInput != Vector2.zero)
+            {
+
+                bool success = TryMove(movementInput);
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(movementInput.x, 0));
+                }
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+                if (movementInput.y > 0 && movementInput.x == 0)
+                {
+                    animator.SetBool("isMovingUp", true);
+                    animator.SetBool("isMoving", false);
+                    animator.SetBool("isMovingDown", false);
+                }
+                else if (movementInput.y < 0 && movementInput.x == 0)
+                {
+                    animator.SetBool("isMovingDown", true);
+                    animator.SetBool("isMoving", false);
+                    animator.SetBool("isMovingUp", false);
+                }
+                else
+                {
+                    animator.SetBool("isMoving", success);
+                    animator.SetBool("isMovingUp", false);
+                    animator.SetBool("isMovingDown", false);
+                }
+
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+                animator.SetBool("isMovingUp", false);
+                animator.SetBool("isMovingDown", false);
+            }
+
+            //Cambiar direccion del sprite segun la direccion en la que se mueva el personaje
+            if (movementInput.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if (movementInput.x > 0)
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+    }
+
+
+
+
+    private bool TryMove(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
+            int count = rb.Cast(
+           direction,
+           movementFilter,
+           castCollisions,
+           moveSpeed * Time.fixedDeltaTime + collisionOffset
+       );
+            direction.Normalize();
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        movementInput = movementValue.Get<Vector2>();
+
+    }
+
+    void OnFire()
+    {
+        animator.SetTrigger("SwordAttack");
+    }
+
+    public void SwordAttack()
+    {
+        LockMovement();
+        if (spriteRenderer.flipX == true)
+        {
+            swordAttack.AttackLeft();
+        }
+        else
+        {
+            swordAttack.AttackRight();
+        }
+    }
+
+    public void EndSwordAttack()
+    {
+        UnlockMovement();
+        swordAttack.StopAttack();
+    }
+
+    public void LockMovement()
+    {
+        canMove = false;
+    }
+
+    public void UnlockMovement()
+    {
+        canMove = true;
+    }
+
 }
