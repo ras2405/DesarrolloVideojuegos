e378c0d175767ad58f279913e9cbbe239a6e7914
Romina Arour <rominar2000@gmail.com>
2024-11-06 14:47:29 -0300

    fixed day trasnition bug

--- LoboEstas/Assets/Scripts/BedInteraction.cs
+++ LoboEstas/Assets/Scripts/BedInteraction.cs
@@ -55,7 +55,6 @@ public class BedInteraction : MonoBehaviour
 
     private void Dormir()
     {
-        MoveCharacterToBed();
         // Incrementa el día actual
         currentDay++;
         // Actualiza el día en CycleDayController
